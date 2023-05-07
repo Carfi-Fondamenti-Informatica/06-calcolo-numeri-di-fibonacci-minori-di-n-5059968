4f84ab922c5a47920f0e635191efcf9be82fc329
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-07 13:52:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
-
-int main () {
- 
+int main() {
     int n=0;
-    int primo=1;
-    int secondo=1;
-    int terzo=0;
     cin>>n;
-    if(n>=2) {
-        cout << primo << endl;
-        cout << secondo << endl;
-
-        for (int i = 2; i < n; i++) {
-            terzo = primo + secondo;
-            primo = secondo;
-            secondo = terzo;
-            cout << terzo << endl;
-        }
-    }
+    int x1=1;
+    int x2=1;
+    int x3=0;
+    if (n>=1){
+    cout<<x1<<endl;
+    cout<<x2<<endl;
+    while(x3<n){
+        x3=x1+x2;
+        x1=x2;
+        x2=x3;
+        cout<<x3<<endl;
+        n--;
+    }}
     return 0;
 }
