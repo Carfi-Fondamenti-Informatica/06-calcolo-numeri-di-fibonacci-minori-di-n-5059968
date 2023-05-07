3fc683152f5d9dd8bb6094647969197fa95fc0be
5059968 <128074987+5059968@users.noreply.github.com>
2023-05-07 12:50:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
-using namespace std;
+int main () {
+    int i;
+    int n=0;
+    int primo=1;
+    int secondo=1;
+    int terzo=0;
+    cin>>n;
+    if(n>=2) {
+        cout << primo << endl;
+        cout << secondo << endl;
 
-int main() {
-   return 0;
+        for (i = 2; i < n; i++) {
+            terzo = primo + secondo;
+            primo = secondo;
+            secondo = terzo;
+            cout << terzo << endl;
+        }
+    }
+    return 0;
 }
